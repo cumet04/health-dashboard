31921ab0947ca80c8637d51024e0ce28219102d4
inomoto <medalhkr@gmail.com>
2024-11-05 00:04:53 +0900

    genvironments整理

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,11 +11,33 @@ Bundler.require(*Rails.groups)
 
 module HealthDashboard
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    config.api_only = true
 
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.api_only = true
+    config.enable_reloading = false
+    config.eager_load = true
+    config.action_controller.perform_caching = true
+
+    config.assume_ssl = true
+    config.force_ssl = true
+
+    config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+    config.log_tags = [ :request_id ]
+    config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+    config.silence_healthcheck_path = "/up"
+
+    config.consider_all_requests_local = false
+    config.active_record.migration_error = :raise
+    config.active_support.deprecation = :log
+    config.active_support.disallowed_deprecation = :log
+    config.active_support.disallowed_deprecation_warnings = []
+
+    config.cache_store = :solid_cache_store
+    config.active_job.queue_adapter = :solid_queue
+    config.solid_queue.connects_to = { database: { writing: :queue } }
+
+    config.active_record.dump_schema_after_migration = false
   end
 end
