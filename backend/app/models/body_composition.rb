3c9c654527b491ccc9b07676d7135b6e6801068e
inomoto <medalhkr@gmail.com>
2025-09-19 21:20:44 +0900

    データ取ってくる部分をモデルから外しておく

--- backend/app/models/body_composition.rb
+++ backend/app/models/body_composition.rb
@@ -12,33 +12,4 @@ class BodyComposition < ApplicationRecord
   def to_display_hash
     attributes.except('id', 'created_at', 'updated_at')
   end
-
-  def self.fetch_raw_records(since)
-    require_relative '../../lib/tanita-rb/lib/tanita'
-
-    tanita = Tanita.new
-
-    tanita.login(
-      ENV.fetch('HEALTHPLANET_ID'),
-      ENV.fetch('HEALTHPLANET_PASSWORD')
-    )
-
-    tanita.fetch_since(since)
-  end
-
-  def self.from_raw_record(record)
-    new(
-      time: record.time,
-      weight: record.values['体重'],
-      body_fat: record.values['体脂肪率'],
-      body_fat_mass: record.values['体脂肪量'],
-      visceral_fat_level: record.values['内臓脂肪レベル'],
-      basal_metabolism: record.values['基礎代謝量'],
-      muscle_mass: record.values['筋肉量'],
-      bone_mass: record.values['推定骨量'],
-      body_age: record.values['体内年齢']&.to_i,
-      body_water_percentage: record.values['体水分率'],
-      muscle_quality_score_all: record.values['筋質点数（全身）']&.to_i
-    )
-  end
 end
